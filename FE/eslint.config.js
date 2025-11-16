a3be680744a627544903f2ecf4220433740b4a07
Alexey Ushakov <develengie@gmail.com>
2025-11-16 17:03:15 +0300

    build(config): created ESLint and Prettier configs

--- FE/eslint.config.js
+++ FE/eslint.config.js
@@ -1,23 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import pluginPrettier from "eslint-plugin-prettier";
+import importPlugin from 'eslint-plugin-import';
+import prettierConfig from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["dist"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: pluginPrettier,
+      import: importPlugin
     },
   },
-])
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      indent: ["error", 4],
+      semi: [2, "always"],
+      "space-before-function-paren": ["error", "never"],
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      "max-len": ["error", { "code": 100 }],
+      "no-console": "error",
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+      ...importPlugin.configs.recommended.rules,
+      "import/no-cycle": "error"
+    },
+  },
+];
