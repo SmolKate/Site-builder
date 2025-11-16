841de663c44c987a91d8710a064128414fc75a30
Alexey Ushakov <develengie@gmail.com>
2025-11-16 17:16:58 +0300

    build(config): created Stylelint config

---
+++ FE/stylelint.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "declaration-empty-line-before": [
+      "always",
+      {
+        except: ["first-nested"],
+        ignore: ["after-declaration"]
+      }
+    ],
+    "rule-empty-line-before": [
+      "always",
+      {
+        except: ["first-nested"],
+        ignore: ["after-comment"]
+      }
+    ],
+    "color-function-alias-notation": "with-alpha",
+    "color-function-notation": "legacy",
+    "alpha-value-notation": "number",
+    "value-keyword-case": ["lower", { camelCaseSvgKeywords: true }],
+    "color-hex-length": "short"
+  }
+};
