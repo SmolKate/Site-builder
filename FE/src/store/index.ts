b3b39c9ba6f35f72e49f42e357f437872866c8d6
Alexey Ushakov <develengie@gmail.com>
2025-11-20 20:02:50 +0300

    feat: #17 добавил RTK Query

--- FE/src/store/index.ts
+++ FE/src/store/index.ts
@@ -1,1 +1,34 @@
-// экспорт стора и хуков для стора
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
+import sitesReducer, { sitesMiddleware, sitesReducerPath } from "./sites";
+
+export const rootReducer = combineReducers({
+  [sitesReducerPath]: sitesReducer,
+});
+
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    devTools: true,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat([
+      sitesMiddleware
+    ]),
+    preloadedState,
+  });
+};
+
+export const store = setupStore();
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
