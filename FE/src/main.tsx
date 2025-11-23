90b167b1bef148f3bae8675b7f975fa4c6ceb777
Jkerdley <jk-erley@yandex.ru>
2025-11-23 20:51:29 +0700

    feat: #43 добавил темизацию на основе контекста

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/main.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ThemeProvider } from "@/context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
