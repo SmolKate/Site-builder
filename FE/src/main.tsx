8d45900157f096d086540dce04143baa9b2b0d4f
Jkerdley <jk-erley@yandex.ru>
2025-11-23 20:47:13 +0700

    feat: #23 добавил radix, модалку и popover

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -9,9 +8,11 @@ import { store } from "./store";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
