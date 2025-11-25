0915ce45547f9598b6f477d601b91088acb1a62f
Смольянинова Екатерина Александровна <e.smolyaninova@tele2.ru>
2025-11-25 23:28:25 +0300

    feat: #41 добавить ui для button

--- FE/src/ui/types.ts
+++ FE/src/ui/types.ts
@@ -5,7 +5,27 @@ const TVariant = {
 
 type TVariantKeys = typeof TVariant[keyof typeof TVariant];
 
+const TButtonVariant = {
+  PRIMARY: "primary",
+  SECONDARY: "special",
+  DANGER: "danger",
+} as const;
+
+type TButtonVariantKeys = typeof TButtonVariant[keyof typeof TButtonVariant];
+
+const TButtonSize = {
+  SMALL: "sm",
+  MEDIUM: "md",
+  LARGE: "lg",
+} as const;
+
+type TButtonSizeKeys = typeof TButtonSize[keyof typeof TButtonSize];
+
 export {
   TVariant,
   type TVariantKeys,
+  TButtonVariant,
+  type TButtonVariantKeys,
+  TButtonSize,
+  type TButtonSizeKeys,
 };
