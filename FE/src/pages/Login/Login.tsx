cde5bd8da9a74022473297c10f0ec5c5d966d161
Alexey Ushakov <develengie@gmail.com>
2025-11-26 17:46:20 +0300

    feat: #26 создал страницу авторизации

--- FE/src/pages/Login/Login.tsx
+++ FE/src/pages/Login/Login.tsx
@@ -1,17 +1,60 @@
 import { useAppDispatch } from "@/store";
 import { login } from "@/store/auth";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, InputField, PasswordField } from "@/ui";
+import { loginSchema, type LoginFormData } from "@/utils/helpers";
+import "../authStyles.scss";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from || "/";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormData>({
+    resolver: yupResolver(loginSchema),
+    mode: "onChange",
+  });
 
-  const handleClickLogin = () => {
+  const onSubmit = () => {
     dispatch(login());
     navigate(from, { replace: true });
   };
 
-  return <button onClick={handleClickLogin}>Login</button>;
+  return (
+    <div className="auth-page">
+      <div className="container">
+        <div className="auth-page__inner">
+          <h2 className="auth-page__title">Войти</h2>
+
+          <form className="auth-page__form" onSubmit={handleSubmit(onSubmit)}>
+            <InputField
+              register={register}
+              errors={errors}
+              fieldName="email"
+              label="Электронная почта"
+              placeholder="Введите электронную почту..."
+            />
+            <PasswordField
+              register={register}
+              errors={errors}
+              fieldName="password"
+              label="Пароль"
+              placeholder="Введите пароль..."
+            />
+            <Button buttonText="Войти" disabled={!isValid} />
+          </form>
+
+          <p className="text-center">
+            Нет аккаунта? <Link to="/signup">Зарегистрироваться</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
